5310702f525bd182d80f60c5aef9150fa32589ec
Nealford Sotes <nealfordsotes@gmail.com>
2021-03-14 22:03:49 +0800

    Version 1?

---
+++ OrderSys/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSys
+{
+    public class Person
+    {
+        public int Cust_ID { get; set; }
+        public string Name { get; set; }
+        public int Contact { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
